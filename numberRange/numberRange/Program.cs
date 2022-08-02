6480060773659fb9c827e6bffc563e5cd965b8ef
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-02 14:10:11 +0300

    is number in 1-100 range
    internship

---
+++ numberRange/numberRange/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number1;
+            Console.WriteLine("Please enter a number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number1 <= 100 && number1 >= 0)
+            {
+                Console.WriteLine("NUMBER IS IN THE RANGE 0-100...");
+            }
+
+            else if (number1 < 0)
+            {
+                Console.WriteLine("NUMBER IS LESS THAN 0, NOT INCLUDED IN THE RANGE...");
+            }
+            else
+            {
+                Console.WriteLine("NUMBER IS GREATER THAN 100, NOT INCLUDED IN THE RANGE...");
+            }
+
+        }
+
+    }
+
+}
